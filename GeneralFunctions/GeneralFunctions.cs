abba1dc9e5c9c1b4d6794a6a85d80a814c0d3df7
Bernat Agulló Roselló <40455069+bernatagulloesbrina@users.noreply.github.com>
2022-12-20 23:36:28 +0100

    Add project files.

---
+++ GeneralFunctions/GeneralFunctions.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabularEditor.TOMWrapper;
+using TabularEditor.Scripting;
+
+
+namespace GeneralFunctions
+{
+    public static class Fx
+    {
+        static readonly Model Model;
+        static readonly TabularEditor.UI.UITreeSelection Selected;
+
+        public static void Error(string message, int lineNumber = -1, bool suppressHeader = false)
+        {
+            ScriptHelper.Error(message: message, lineNumber: lineNumber, suppressHeader: suppressHeader);
+        }
+
+        public static void Info(string message, int lineNumber = -1)
+        {
+            ScriptHelper.Info(message: message, lineNumber: lineNumber);
+        }
+
+        public static Table SelectTable(IEnumerable<Table> tables, Table preselect, string label)
+        {
+            return ScriptHelper.SelectTable(tables: tables, preselect: preselect, label: label);
+        }
+
+        public static void Output(object value, int lineNumber = -1)
+        {
+            ScriptHelper.Output(value: value, lineNumber: lineNumber);
+        }
+
+        //public static class Fx { 
+
+        public static Table CreateCalcTable(
+        string tableName = "Measures",
+        string tableExpression = "FILTER({BLANK()},FALSE)")
+        {
+            if (Model.Tables.Any(t => t.Name == tableName))
+            {
+                Error("There is already a table called " + tableName + ".");
+                return null;
+            };
+
+            Table newTable =
+                Model.AddCalculatedTable(
+                    name: tableName,
+                    expression: tableExpression);
+
+            return newTable;
+
+        }
+
+
+
+        //Func<Table, bool> mTables = (t) => !model.Relationships.Any(r => r.ToTable == t || r.FromTable == t)
+        //    //the table has no visible columns
+        //    && t.Columns.All(c => !c.IsVisible);
+
+
+        public static Table GetMeasureTable(string tableName = "Measures", bool createIfNecessary = true)
+        {
+
+
+            var MeasureTables = Model.Tables.Where
+                    (t =>
+                        //no relationships start or end on the table
+                        !Model.Relationships.Any(r => r.ToTable == t || r.FromTable == t)
+                        //the table has no visible columns
+                        && t.Columns.All(c => !c.IsVisible)
+                    );
+            if (MeasureTables.Count() == 0)
+            {
+                if (createIfNecessary)
+                {
+                    //does not exist, new one is created
+                    Table newMeasureTable =
+                        Model.AddCalculatedTable(
+                            name: tableName,
+                            expression: "FILTER({BLANK()},FALSE)"
+                        );
+                    return newMeasureTable;
+
+                }
+                else
+                {
+                    //does not exist, will not create
+                    return null;
+                };
+            }
+            else if (MeasureTables.Count() == 1)
+            {
+                Table singleExistingTable = MeasureTables.First();
+                return singleExistingTable;
+            }
+            else
+            {
+                Table chosenTable = ScriptHelper.SelectTable(MeasureTables, MeasureTables.First(), "Select measure table to use");
+            }
+
+            return null;
+        }
+
+
+    }
+}
---
+++ TE Scripts/TE_Scripts.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabularEditor.TOMWrapper;
+using TabularEditor.Scripting;
+//using GeneralFunctions;
+
+namespace TE_Scripting
+{
+    public class TE_Scripts
+    {
+
+        static readonly Model Model;
+        static readonly TabularEditor.UI.UITreeSelection Selected;
+
+        public static void Error(string message, int lineNumber = -1, bool suppressHeader = false)
+        {
+            ScriptHelper.Error(message: message, lineNumber: lineNumber, suppressHeader: suppressHeader);
+        }
+
+        public static void Info(string message, int lineNumber = -1)
+        {
+            ScriptHelper.Info(message: message, lineNumber: lineNumber);
+        }
+
+        public static Table SelectTable(IEnumerable<Table> tables, Table preselect = null, string label = "Select Table")
+        {
+            return ScriptHelper.SelectTable(tables: tables, preselect: preselect, label: label);
+        }
+
+        public static Column SelectColumn(Table table, Column preselect = null, string label = "Select Column")
+        {
+            return ScriptHelper.SelectColumn(table: table, preselect: preselect, label: label); 
+        }
+
+        public static void Output(object value, int lineNumber = -1)
+        {
+            ScriptHelper.Output(value: value, lineNumber: lineNumber);
+        }
+
+        void createLabelCalcGroup()
+        {
+            String calcGroupName = "Labels 2";
+
+            CalculationGroupTable cg = Model.AddCalculationGroup(name: calcGroupName);
+
+            Table t = SelectTable(Model.Tables, label:"Select table of axis field");
+            if (t == null) return;
+
+            Column c = SelectColumn(table:t,label:"Select axis field");
+            if(c == null) return;
+
+            String calcItemName = "Only Top Value";
+
+            String calcItemExpression = 
+                String.Format(
+                    @"VAR maxValue =
+                        CALCULATE(
+                            MAXX(
+                                VALUES(
+                                    {0}
+                                ),
+                                SELECTEDMEASURE( )
+                            ),
+                            ALLSELECTED( {1} )
+                        )
+                    VAR currentValue =
+                        SELECTEDMEASURE( )
+                    VAR fString =
+                        IF(
+                            maxValue = currentValue,
+                            SELECTEDMEASUREFORMATSTRING(
+
+                            ),
+                            "";;;""
+                        )
+                    RETURN
+                        fString",c.DaxObjectFullName,t.DaxObjectFullName);
+
+            CalculationItem ci = cg.AddCalculationItem(name: calcItemName, expression: calcItemExpression);
+            ci.FormatDax();
+
+
+
+        }
+
+    }
+}
